6f6cd96f9cbbbd691bc99cf10823c316535d807f
HasakiYoYo <49520743+HasakiYoYo@users.noreply.github.com>
2020-04-14 13:35:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/vernesong/OpenClash                                 package/clash
+git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
+git clone https://github.com/frainzy1477/luci-app-clash           package/luci-app-clash
+git clone https://github.com/jerrykuku/luci-theme-argon           package/luci-theme-argon
