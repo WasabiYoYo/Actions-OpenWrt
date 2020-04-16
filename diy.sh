0d11821f1563ebc5806364ab13bc8960fc2aab19
WasabiYoYo <61608688+WasabiYoYo@users.noreply.github.com>
2020-04-16 10:53:59 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
-src-git lienol https://github.com/Lienol/openwrt-package
 git clone https://github.com/vernesong/OpenClash                                 package/clash
 git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
 # git clone https://github.com/frainzy1477/luci-app-clash           package/luci-app-clash
