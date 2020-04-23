5c938d2ab5ad72f50f8d0bcde6f9270f585a41a6
WasabiYoYo <61608688+WasabiYoYo@users.noreply.github.com>
2020-04-23 22:23:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,13 +7,14 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
-# src-git lienol https://github.com/Lienol/openwrt-package
-# git clone https://github.com/vernesong/OpenClash                                 package/clash
+src-git lienol https://github.com/Lienol/openwrt-package
+git clone https://github.com/vernesong/OpenClash                                 package/clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome                  package/adguardhome
-# git clone https://github.com/tzxiaozhen88/koolproxyR                         package/koolproxyR
+git clone https://github.com/tzxiaozhen88/koolproxyR                         package/koolproxyR
 git clone https://github.com/ElonH/Rclone-OpenWrt                 package/rclone
 git clone https://github.com/frainzy1477/luci-app-clash           package/luci-app-clash
-# git clone https://github.com/jerrykuku/luci-theme-argon           package/luci-theme-argon
-# git clone https://github.com/pymumu/luci-app-smartdns                      package/luci-app-smartdns
-# git clone https://github.com/pymumu/openwrt-smartdns                       package/openwrt-smartdns
-
+git clone https://github.com/jerrykuku/luci-theme-argon           package/luci-theme-argon git clone https://github.com/pymumu/luci-app-smartdns                      package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns                       package/openwrt-smartdns
+git clone https://github.com/HasakiYoYo/helloworld                        package/helloword
+./scripts/feeds update -a
+./scripts/feeds install -a
